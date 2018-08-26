5daccbe2e48b7bcf401eef62c2f385ffb5cf0e87
Laszlo Kovesdi <lacikovesdi@gmail.com>
2018-08-26 13:39:18 -0700

    added footer and header components

---
+++ my-app/src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../../App.css';
+
+class Footer extends Component {
+    render() {
+        return (
+            <h1 className="App-title">Footer</h1>
+        )
+    }
+}
+
+export default Footer;
