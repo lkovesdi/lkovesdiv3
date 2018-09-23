1d1d176bd5722f6ec01fac9ca1df6958ff0f3de4
Laszlo Kovesdi <lacikovesdi@gmail.com>
2018-09-23 08:47:46 -0700

    1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import BackgroundImage from 'react-background-image-loader';
+import HomepageLayout from "./components/homepageDir/homepage";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App">
+         <BackgroundImage className="bg" src="">
+                    <HomepageLayout />
+         </BackgroundImage>
       </div>
     );
   }
 }
 
 export default App;
+
+
